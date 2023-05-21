e917a7d66ccb9e43e44c76476a158b55f4edd996
unknown <lucas.sjmsilva@gmail.com>
2023-05-20 23:19:05 -0300

    Adding Controller of Categories and Articles

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const connection = require("./database/database")
+const categoriesController = require("./categories/CategoriesController")
+const articlesController = require("./articles/ArticlesController")
 
 //View engine
 app.set('view engine', 'ejs')
@@ -21,6 +23,11 @@ connection
     }).catch((error) => {
         console.log(error)
     })
+
+
+app.use("/", categoriesController)
+
+app.use("/", articlesController)
 
 app.get("/", (req,res) =>{
     res.render("index")
