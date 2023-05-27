76a146b42c9885578c8859ed8f2a71603ea900a3
Lucas Jesus <lucas.sjmsilva@gmail.com>
2023-05-27 13:45:10 -0300

    following up with the project

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -6,7 +6,7 @@ router.get("/categories", (req,res) => {
 })
 
 router.get("/admin/categories/new", (req,res) => {
-    res.send("Rota para criar uma nova categoria")
+    res.render("./admin/categories/new")
 })
 
 module.exports = router
