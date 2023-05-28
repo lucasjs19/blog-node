c8619dd5054659ca7736ef5b7020cb24a7e526e9
Lucas Jesus <lucas.sjmsilva@gmail.com>
2023-05-27 22:47:04 -0300

    Commiting the pages for create and edit a category

--- database/database.js
+++ database/database.js
@@ -2,7 +2,11 @@ const Sequelize = require("sequelize")
 
 const connection = new Sequelize('blog', 'root', '123456',{
     host:'localhost',
-    dialect: 'mysql' 
+    dialect: 'mysql',
+    timezone: '-03:00',
+    dialectOptions: {
+        useUTC: false
+    }
 })
 
 module.exports = connection;
